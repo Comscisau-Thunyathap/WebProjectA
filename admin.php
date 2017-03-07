a91bac60db54139663eca0f640e3c8ff38d5924f
HexEnophy <Je_Deakthap@hotmail.com>
2017-03-07 15:47:22 +0700

    Update 2

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Admin Page</title>
+</head>
+<body>
+	<center>
+		<h1>Admin</h1>
+	</center>
+	
+</body>
+</html>
---
+++ signinprocess.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sign In Process</title>
+</head>
+<body>
+<?php
+		//เขียนข้อมูล PHP เอาข้อมูลทีส่งไป Select ใน DB ว่ามีหรือไม่ ถ้าไม่มีแสดงข้อความบอก ถ้ามีต้องดูว่าเป้น admin->admin.php หรือ member->member.php
+
+		$uname = $_POST['uname'];
+		$upassword = $_POST['upassword'];
+		
+?>	
+</body>
+</html>
